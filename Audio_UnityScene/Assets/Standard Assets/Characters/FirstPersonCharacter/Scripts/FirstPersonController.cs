4b64007a3b50cd8bf18560d826b2234f73e15da9
paupedra <paupe200@gmail.com>
2021-05-14 19:38:08 +0200

    Background music, birds, mixers

--- Audio_UnityScene/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Audio_UnityScene/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -291,22 +291,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void OnControllerColliderHit(ControllerColliderHit hit)
         {
-            Rigidbody body = hit.collider.attachedRigidbody;
-            //dont move the rigidbody if the character is on top of it
-            if (m_CollisionFlags == CollisionFlags.Below)
-            {
-                return;
-            }
-
-            if (body == null || body.isKinematic)
-            {
-                return;
-            }
-            body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
-        }
-        void OnCollisionEnter(Collision coll)
-        {
-            string tag = coll.gameObject.tag;
+            string tag = hit.gameObject.tag;
 
             switch (tag)
             {
@@ -320,6 +305,19 @@ namespace UnityStandardAssets.Characters.FirstPerson
                     footstepMaterial = tag;
                     break;
             }
+
+            Rigidbody body = hit.collider.attachedRigidbody;
+            //dont move the rigidbody if the character is on top of it
+            if (m_CollisionFlags == CollisionFlags.Below)
+            {
+                return;
+            }
+
+            if (body == null || body.isKinematic)
+            {
+                return;
+            }
+            body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
     }
 }
